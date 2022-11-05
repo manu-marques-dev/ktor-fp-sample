8ccb3af24231efdfd0100156ffabdd81b0bc40a3
manuel.marques.contractor <manuel.marques.contractor@bbva.com>
2022-11-05 12:31:13 +0100

    add coroutines and arrow dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 val ktorVersion: String by project
 val kotlinVersion: String by project
 val logbackVersion: String by project
+val coroutinesVersion: String by project
+val arrowVersion: String by project
 
 plugins {
     application
     kotlin("jvm") version "1.7.20"
     id("io.ktor.plugin") version "2.1.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20"
+    id("com.google.devtools.ksp") version "1.7.20-1.0.7"
 }
 
 group = "com.example"
@@ -28,6 +31,18 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
+    implementation("io.arrow-kt:arrow-optics:$arrowVersion")
+    implementation("io.arrow-kt:arrow-fx-coroutines:$arrowVersion")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+
+    ksp("io.arrow-kt:arrow-optics-ksp-plugin:$arrowVersion")
 }
+
+
+
+
+
